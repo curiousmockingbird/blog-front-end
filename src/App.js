ebee207ca56421d6745314663bdbf132e0628397
Harold_Mesa <harold.mesa@icloud.com>
2022-12-10 00:09:51 -0600

    Add useEffect to handle api request from client side.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import SignIn from './SignIn';
-import BlogControl from './BlogControl';
+// import SignIn from './SignIn';
+// import BlogControl from './BlogControl';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-function App() {
+const App = () => {
+  const [articleInfo, setArticleInfo] = useState(0);
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    }
+    loadArticleInfo();
+  });
+
   return (
-    <Router>
-    <Routes>
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/" element={<BlogControl />} />
-    </Routes>
-  </Router>
+    <h1>{articleInfo}</h1>
+  //   <Router>
+  //   <Routes>
+  //     <Route path="/sign-in" element={<SignIn />} />
+  //     <Route path="/" element={<BlogControl />} />
+  //   </Routes>
+  // </Router>
   );
 }
 
