7e32bed24354dd773d041ea524260c812532828f
Harold_Mesa <harold.mesa@icloud.com>
2022-12-10 15:31:46 -0600

    WIP: Formatting blog sign-in/sign-up page.

--- src/components/App.js
+++ src/components/App.js
@@ -2,34 +2,13 @@ import React from 'react';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 // import BlogControl from './BlogControl';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogInHeader from './LogInHeader';
 
 const App = () => {
-  const [articleInfo, setArticleInfo] = useState(0);
-
-  //Automatically fetches articles count every 10s from database through endpoint api/articles
-  useEffect(() => {
-    const loadArticleInfo = async () => {
-      const response = await axios.get(`/api/articles/`);
-      const newArticleInfo = response.data;
-      setArticleInfo(newArticleInfo);
-    }
-    loadArticleInfo();
-
-    const interval = setInterval(() => {
-      loadArticleInfo()
-    }, 10000);
-    //The interval is cleared when the user navigates to another page
-    return()=>clearInterval(interval);
-
-  }, []);
 
   return (
     <Router>
-    <h1>{articleInfo}</h1>
     <LogInHeader />
     <Routes>
       <Route path="/sign-in" element={<SignIn />} />
--- src/components/LogInHeader.js
+++ src/components/LogInHeader.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from "react-router-dom";
+import './../css/LogInHeader.css';
 
 const LogInHeader = () => {
+  const [articleCount, setArticleCount] = useState(0);
+
+  //Automatically fetches articles count every 10s from database through endpoint api/articles
+  useEffect(() => {
+    const loadArticleCount = async () => {
+      const response = await axios.get(`/api/articles/`);
+      const newArticleCount = response.data;
+      setArticleCount(newArticleCount);
+    }
+    loadArticleCount();
+
+    const interval = setInterval(() => {
+      loadArticleCount()
+    }, 10000);
+    //The interval is cleared when the user navigates to another page
+    return()=>clearInterval(interval);
+
+  }, []);
   return (
-    <React.Fragment>
-      <h1> Help Queue</h1>
+    
+      <div className='test container'>
+      <h1> Welcome to the Cuban blog!</h1>
+      <h3>{articleCount}</h3>
+      <h4>Articles published, and counting</h4>
       <ul>
         <li>
           <Link to="/sign-up">Sign Up</Link>
@@ -13,7 +37,7 @@ const LogInHeader = () => {
           <Link to="/sign-in">Sign In</Link>
         </li>
       </ul>
-    </React.Fragment>
+      </div>
   );
 }
 export default LogInHeader
