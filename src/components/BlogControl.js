4fca4b07457015707a5165de757aafe2163f7785
Harold_Mesa <harold.mesa@icloud.com>
2022-12-12 17:01:43 -0600

    Add Firebase authentication.

--- src/components/BlogControl.js
+++ src/components/BlogControl.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useState, useEffect } from 'react';
+// import { db, auth } from './../firebase';
 import axios from 'axios';
 import './../css/LogInHeader.css';
 import { Link } from 'react-router-dom';
+import useUser from './../hooks/useUser';
+
 const BlogControl = () => {
 
   const [articleCount, setArticleCount] = useState(0);
+  const { user, isLoading } = useUser();
 
   //Automatically fetches articles count every 10s from database through endpoint api/articles
   useEffect(() => {
@@ -23,25 +27,31 @@ const BlogControl = () => {
     // return()=>clearInterval(interval);
   }, []);
 
-  const handleClick = () => {
-    // setLogInHeaderVisible(false);
+  if (!user) {
+    return (
+      <section className='container'>
+      <div className="test">
+        <h1> Welcome to the Cuban blog!</h1>
+        <h3>{articleCount}</h3>
+        <h4>Articles published, and counting</h4>
+        <ul>
+          <li>
+            <Link to='/sign-in'>{<button>Sign In / Sign Up</button>}</Link>
+          </li>
+        </ul>
+      </div>
+      </section>
+    )
+  } else if (user) {
+
+    return (      
+      <section className='container'>
+      <div className="test">
+        <h1> Welcome to the Cuban blog <br></br>{user.email}!</h1>
+      </div>
+      </section>
+    )
   }
-  return (
-    <>
-    <section className='container'>
-    <div className="test">
-      <h1> Welcome to the Cuban blog!</h1>
-      <h3>{articleCount}</h3>
-      <h4>Articles published, and counting</h4>
-      <ul>
-        <li>
-          <Link to='/sign-in'>{<button onClick={handleClick}>Sign In / Sign Up</button>}</Link>
-        </li>
-      </ul>
-    </div>
-    </section>
-    </>
-  )
 }
 
 export default BlogControl;
