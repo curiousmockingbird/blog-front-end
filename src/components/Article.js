e454710488be629968fc76e4ef91ba27a1f16e4e
Harold_Mesa <harold.mesa@icloud.com>
2022-12-16 21:30:30 -0600

    WIP: Working on content tags

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,23 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
   
 function Article(props) {
-  
+  // Conditional rendering of tags
+  let tagContent = null;
+  if (props.tags.length > 1) {
+    tagContent = props.tags.map((tag) => (
+      `#${tag} `
+      ));
+  } else {
+    tagContent = `#${props.tags}`;
+  }
+  console.log(tagContent[tagContent.length-1].slice(0,-1));
+
   return (
     <div className='article'>
       <hr />
       <h3>{props.title}</h3>
       <p>{props.content}</p>
-      foreach (props.tags as tag) {
-        <span className='tag'>{tag}</span> 
-      }
-      
+      <p>{tagContent}</p>
     </div>
   );
 }
 
 Article.prototype = {
   title: PropTypes.string,
-  content: PropTypes.string
+  content: PropTypes.string,
+  tags: PropTypes.string
 };
 
 export default Article;
